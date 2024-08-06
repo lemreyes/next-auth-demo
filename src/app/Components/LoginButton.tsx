ca992bb2f228e57ca7d7d9161649a311d528c636
Lemuel Leogene Reyes <lemuelp.reyes@gmail.com>
2024-08-06 15:08:21 +0800

    google provider

---
+++ src/app/Components/LoginButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function LoginButton() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user?.email} <br />
+        <button
+          onClick={() => signOut()}
+          className="bg-gray-400 px-4 py-2 rounded-lg"
+        >
+          Sign out
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <p>Not signed in</p>
+        <button
+          onClick={() => signIn()}
+          className="bg-gray-400 px-4 py-2 rounded-lg"
+        >
+          Sign in
+        </button>
+      </>
+    );
+  }
+}
