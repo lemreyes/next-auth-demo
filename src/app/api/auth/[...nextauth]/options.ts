cbf86770f5a8b8ff4d68de5c43c25ba7d92d2560
Lemuel Leogene Reyes <lemuelp.reyes@gmail.com>
2024-08-17 19:26:38 +0800

    add database adapter schema and options

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,9 +1,12 @@
 import type { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/app/Utilities/prismaUtil";
 
 import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 
 export const options: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
