87bf6165e9321e350afd6933ab91a0e3958d4eab
Lemuel Leogene Reyes <lemuelp.reyes@gmail.com>
2024-08-18 09:43:26 +0800

    login with email UI

--- src/app/Components/LoginButton.tsx
+++ src/app/Components/LoginButton.tsx
@@ -1,8 +1,14 @@
 "use client";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { ChangeEvent, useState } from "react";
 
 export default function LoginButton() {
   const { data: session } = useSession();
+  const [email, setEmail] = useState("");
+
+  const hdlEmailInputOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
 
   if (session) {
     return (
@@ -18,7 +24,7 @@ export default function LoginButton() {
     );
   } else {
     return (
-      <>
+      <div>
         <p>Not signed in.</p>
         <button
           onClick={() => signIn("google", { callbackUrl: "/" })}
@@ -26,7 +32,16 @@ export default function LoginButton() {
         >
           Sign in to Google
         </button>
-      </>
+        <p>or</p>
+        <label htmlFor="email">Email: </label>
+        <input type="text" id="email" onChange={hdlEmailInputOnChange} className="border border-black mr-2"></input>
+        <button
+          className="bg-gray-400 px-4 py-2 rounded-lg"
+          onClick={() => signIn("email", { email, callbackUrl: "/" })}
+        >
+          Login with Email
+        </button>
+      </div>
     );
   }
 }
